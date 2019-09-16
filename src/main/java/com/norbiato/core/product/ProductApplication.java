c706937a4853b3e1ac618ec5af3645147319730a
Caio Norbiato <caio@norbiato.net>
2019-09-15 22:10:13 -0300

    Ajustes PostgreSQL 11

--- src/main/java/com/norbiato/core/product/ProductApplication.java
+++ src/main/java/com/norbiato/core/product/ProductApplication.java
@@ -1,11 +1,11 @@
 package com.norbiato.core.product;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
+@EnableAutoConfiguration
 public class ProductApplication {
 
     public static void main(String[] args) {
